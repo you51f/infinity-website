279ecf51e57e2ced6952b230b2fff6df47f6f301
You5if <yousif.ahmed@markoncs.com>
2023-09-02 03:32:16 +0200

    second commit

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -10,35 +10,16 @@ export async function POST(request) {
   const body = await request.json()
   // console.log(body);
   try {
-    if (body.subject != "Spare parts") {
+    if (body.subject === "Spare parts") {
       const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
       port: 587,
       secure: false,
         auth: {
-            // user: "Sales.infinityaviation@gmail.com",
-            // pass: "fywqwhcxrubckben"
-            user: email,
-            pass: password
-        },
-    })
-      await transporter.sendMail({
-        from: email,
-        to: email,
-        subject: body.subject,
-        text: "A message from infinity website",
-        html: `<h2>Company: ${body.company}</h2><p><h3>Customer: ${body.firstName} ${body.lastName}</h3> </p><p>${body.email}</p><p>${body.phone}</p><h4>Message:</h4><p>${body.comment}</p>`
-      })
-    }else {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-        auth: {
-            // user: "Consulting.infinityaviation@gmail.com",
-            // pass: "tgyfqeorwxohjnue"
-            user: email,
-            pass: password
+            user: "Consulting.infinityaviation@gmail.com",
+            pass: "tgyfqeorwxohjnue"
+            // user: email,
+            // pass: password
         },
     })
       await transporter.sendMail({
@@ -47,6 +28,44 @@ export async function POST(request) {
         subject: body.subject,
         text: "A message from infinity website",
         html: `<h2>Company: ${body.company}</h2><p><h3>Customer: ${body.firstName} ${body.lastName}</h3> </p><p>${body.email}</p><p>${body.phone}</p><h3>Ordered Part: ${body.part}</h3><h3>Quantity: ${body.quantity}</h3><h3>Condition: ${body.condition}</h3><h3>Message:</h3><p style="white-space: pre-wrap">${body.comment}</p>`
+      })
+    }else if (body.subject === "Spare parts") {
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+        auth: {
+            user: "Consulting.infinityaviation@gmail.com",
+            pass: "tgyfqeorwxohjnue"
+            // user: email,
+            // pass: password
+        },
+    })
+      await transporter.sendMail({
+        from: email,
+        to: email,
+        subject: body.subject,
+        text: "A message from infinity website",
+        html: `<h2>Company: ${body.company}</h2><p><h3>Customer: ${body.firstName} ${body.lastName}</h3> </p><p>${body.email}</p><p>${body.phone}</p><h3>Ordered Parts:</h3><p style="white-space: pre-wrap">${body.parts}</p><h3>Message:</h3><p style="white-space: pre-wrap">${body.comment}</p>`
+      })
+    }else {
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+        auth: {
+            user: "Sales.infinityaviation@gmail.com",
+            pass: "fywqwhcxrubckben"
+            // user: email,
+            // pass: password
+        },
+    })
+      await transporter.sendMail({
+        from: email,
+        to: email,
+        subject: body.subject,
+        text: "A message from infinity website",
+        html: `<h2>Company: ${body.company}</h2><p><h3>Customer: ${body.firstName} ${body.lastName}</h3> </p><p>${body.email}</p><p>${body.phone}</p><h4>Message:</h4><p>${body.comment}</p>`
       })
     }
 
