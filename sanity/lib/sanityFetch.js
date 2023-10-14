3b2a143c7599b37c58adae9181ea8e10e4e93820
You5if <yousif.ahmed@markoncs.com>
2023-10-14 04:46:59 +0200

    suppliers and sanity setup changes error fixed2 commit

--- sanity/lib/sanityFetch.js
+++ sanity/lib/sanityFetch.js
@@ -9,24 +9,25 @@ export const token = '';
 
 export async function sanityFetch(query, params = DEFAULT_PARAMS, tags = DEFAULT_TAGS) {
   // const isDraftMode = draftMode().isEnabled;
-  if (!token) {
-    throw new Error(
-      "The `SANITY_API_READ_TOKEN` environment variable is required."
-    );
-  }
-  const isDevelopment = process.env.NODE_ENV === "development";
+  // if (!token) {
+  //   throw new Error(
+  //     "The `SANITY_API_READ_TOKEN` environment variable is required."
+  //   );
+  // }
+  // const isDevelopment = process.env.NODE_ENV === "development";
 
-  return client
-    .withConfig({ useCdn: true })
-    .fetch(query, params, {
-      cache: isDevelopment || isDraftMode ? undefined : "force-cache",
-      ...({
-        token: token,
-        perspective: "previewDrafts",
-      }),
-      next: {
-        ...({ revalidate: 30 }),
-        tags,
-      },
-    });
+  // return client
+  //   .withConfig({ useCdn: true })
+  //   .fetch(query, params, {
+  //     cache: isDevelopment || isDraftMode ? undefined : "force-cache",
+  //     ...({
+  //       token: token,
+  //       perspective: "previewDrafts",
+  //     }),
+  //     next: {
+  //       ...({ revalidate: 30 }),
+  //       tags,
+  //     },
+  //   });
+  return 0;
 }
